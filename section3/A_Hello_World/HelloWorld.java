d245cfd294de921bd081af9892683a209ae928b7
Bar Snir <bars@Admins-MacBook-Pro.local>
2022-01-23 17:59:14 +0200

    stoped in lecture 23 section 3

---
+++ section3/A_Hello_World/HelloWorld.java
@@ -0,0 +1,11 @@
+package section3.A_Hello_World;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        logger("Hello World");
+    }
+
+    public static void logger(String msg){
+        System.out.println(msg);
+    }
+}
---
+++ section3/B_Variables/Variables.java
@@ -0,0 +1,9 @@
+package section3.B_Variables;
+
+public class Variables {
+    
+    public static void main(String[] args) {
+        int myFirstInt = 15 * 3;
+        System.out.println(myFirstInt);
+    }
+}
